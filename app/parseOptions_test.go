daf4b45dad0b1de6fa05ef6b76f49d546f310526
opsaaaaa <sean@ferney.org>
2023-09-26 11:24:16 -0600

    cleanup

--- app/parseOptions_test.go
+++ app/parseOptions_test.go
@@ -6,22 +6,18 @@ import (
   "github.com/opsaaaaa/bplate.md/spec"
 )
 
-func Test_parseOptions(t *testing.T) {
-  happyPath := func () {
-    o, err := parseOptions("example.md","path: _posts\nslug: \"{{ date }}.md\"\ncommand: [one, two, three]", []string{"1","2","3"})
+func Test_parseOptions_happyPath(t *testing.T) {
+  o, err := parseOptions("example.md","path: _posts\nslug: \"{{ date }}.md\"\ncommand: [one, two, three]", []string{"1","2","3"})
 
-    spec.AssertErrorNil(t, err)
-    spec.AssertSame(t, o.Path, "_posts")
-    spec.AssertSame(t, len(o.Command), 3)
-    spec.AssertSame(t, len(o.Args), 3)
-  }
-  happyPath()
+  spec.AssertErrorNil(t, err)
+  spec.AssertSame(t, o.Path, "_posts")
+  spec.AssertSame(t, len(o.Command), 3)
+  spec.AssertSame(t, len(o.Args), 3)
+}
 
-  emptyTxt := func () {
-    o, _ := parseOptions("blank","", []string{})
-    spec.AssertSame(t, o.Path, "./")
-  }
-  emptyTxt()
+func Test_parseOptions_emptyTxt(t *testing.T) {
+  o, _ := parseOptions("blank","", []string{})
+  spec.AssertSame(t, o.Path, "./")
 }
 
 
